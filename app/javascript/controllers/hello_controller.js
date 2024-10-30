21c9a432f42c76e8012f21f1793a596f776bdc61
Harshan890 <Harshanahmad@gmail.com>
2024-10-31 01:38:12 +0500

    Added

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Harshan Ahmad"
+    this.element.textContent = "Harshan Ahmad is a good boy."
   }
 }
