2b02678dc7db194b145467beaf8ab1765f9b6ed4
harshan890 <harshanahmad@gmail.com>
2024-10-31 13:33:33 +0500

    Update hello_controller.js

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Harshan Ahmad is a good boy and live in Lahore and a good boy"
+    this.element.textContent = "Harshan Ahmad is a good boy and live in Lahore and a good boy and he is 27 years old."
   }
 }
